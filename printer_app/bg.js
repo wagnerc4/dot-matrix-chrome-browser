e13d0832bc57de55d435a8ad9ab9c658e5ff6123
wagner <wagnerc4@gmail.com>
2017-03-06 11:11:12 -0600

    Update bg.js

--- printer_app/bg.js
+++ printer_app/bg.js
@@ -1,4 +1,4 @@
-// App Launched                                                                                                                                                                                        
+// App Launched
 chrome.app.runtime.onLaunched.addListener(function(data) {
   chrome.app.window.create('index.html', {id: 'print_app', hidden: true});
 });

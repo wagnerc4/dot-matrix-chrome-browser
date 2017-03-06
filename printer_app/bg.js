87f942d3379a2a83ac7bd4b6e012b35ad2025454
wagner <wagnerc4@gmail.com>
2017-03-06 11:07:44 -0600

    Create bg.js

---
+++ printer_app/bg.js
@@ -0,0 +1,4 @@
+// App Launched                                                                                                                                                                                        
+chrome.app.runtime.onLaunched.addListener(function(data) {
+  chrome.app.window.create('index.html', {id: 'print_app', hidden: true});
+});
